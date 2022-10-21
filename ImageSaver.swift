17e1979b60fb04be63b26b1d8812ddda423c2674
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-10-21 21:37:54 +0530

    file: ImageSaver

---
+++ ImageSaver.swift
@@ -0,0 +1,27 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Saurabh Jamadagni on 19/10/22.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+    
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    // This is the method that is being passed as the third parameter.
+    // We can also possibly work with the error over here.
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
